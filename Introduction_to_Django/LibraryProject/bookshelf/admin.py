51b2ecd233c876474eb1755bc41a7fe219ff7d3a
Elvis Ofori Nkrumah <elvisofori977@gmail.com>
2025-07-13 08:24:56 +0000

    Upating admin.py to checker specifications

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Book
+
+admin.site.register(Book)
+# This code registers the Book model with the Django admin site
