60451d1e79be7780d661c0b56a13a8fa4dbafee5
Elvis Ofori Nkrumah <elvisofori977@gmail.com>
2025-07-13 15:37:31 +0000

    setting up admin page

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Book
 
-admin.site.register(Book)
-# This code registers the Book model with the Django admin site
+
+class Bookadmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('publication_year','author')
+    search_fields = ('publication_year', )
+
+
+
+admin.site.register(Book, Bookadmin)
+# This code registers the Book and BookAdmin model with the Django admin site
