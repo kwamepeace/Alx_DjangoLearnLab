16b08543d218f4b9717414d4a3e7809d7a719b54
Elvis Ofori Nkrumah <elvisofori977@gmail.com>
2025-07-19 16:25:48 +0000

    Adding __str__ methods

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Author, Book, Library, Librarian
 
 # Register your models here.
+class LibraryAdmin(admin.ModelAdmin):
+    list_display = ("name", )
+
+
+admin.site.register(Library, LibraryAdmin)
--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField (max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 
 class Book(models.Model):
@@ -11,14 +14,21 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete = models.CASCADE)
 
 
+    def __str__(self):
+        return self.title
+
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book, related_name='libraries')
 
 
+    def __str__(self):
+        return self.name
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
