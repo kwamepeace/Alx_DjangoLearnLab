9c0f5b42ceca4dbb7b95b5ff3002cd04d8bd86da
Elvis Ofori Nkrumah <elvisofori977@gmail.com>
2025-07-20 19:50:26 +0000

    add urls path

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include # Import include
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('relations/', include('relationship_app.urls', namespace='relationship_app')),
+]
