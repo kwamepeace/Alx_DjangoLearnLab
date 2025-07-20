431c0561650603855b67e3d161c4e71d306b205d
Elvis Ofori Nkrumah <elvisofori977@gmail.com>
2025-07-20 22:55:09 +0000

    implementing user authentication method

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,16 +1,20 @@
-from django.contrib import admin
-from django.urls import path, include 
-from django.contrib.auth import views as auth_views
-from .views import list_books, LibraryDetailView,  register_view, CustomLoginView, CustomLogoutView
+from django.urls import path
+from django.contrib.auth import views as auth_views 
+from .views import book_list_view, LibraryDetailView, register_view
 
-app_name = 'relationship_app'
+app_name = 'relationship_app' 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('relations/', include('relationship_app.urls', namespace='relationship_app')),
+    path('books/', book_list_view, name='book_list'),
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('register/', register_view, name='register'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='relationship_app/login.html',
+        redirect_authenticated_user=True 
+    ), name='login'),
 
+    path('logout/', auth_views.LogoutView.as_view(
+        template_name='relationship_app/logout.html',
+        next_page='relationship_app:login' 
+    ), name='logout'),
 ]
