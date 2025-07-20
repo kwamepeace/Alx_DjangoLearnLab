045dff9bd52aca71cc04844ee5dc09f0b178d1db
Elvis Ofori Nkrumah <elvisofori977@gmail.com>
2025-07-20 19:14:21 +0000

    Adding data to query.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,7 +2,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 
 def query_book_by_author(author_name):
-    selected_author = ["Author.objects.get(name=author_name)", "objects.filter(author = author)"]
+    selected_author = Author.objects.get(name=author_name)
     return f"{selected_author.name} has written the following books: {', '.join(book.title for book in selected_author.book_set.all())}"
 
 
