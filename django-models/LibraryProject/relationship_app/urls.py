59ba9be9075ce235361dba7219141754ba0f6cc3
Elvis Ofori Nkrumah <elvisofori977@gmail.com>
2025-07-20 22:38:53 +0000

    implementing user authentication method

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include 
-from .views import list_books, LibraryDetailView
+from django.contrib.auth import views as auth_views
+from .views import list_books, LibraryDetailView,  register_view, CustomLoginView, CustomLogoutView
 
-
+app_name = 'relationship_app'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('relations/', include('relationship_app.urls', namespace='relationship_app')),
+    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', register_view, name='register'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
 ]
