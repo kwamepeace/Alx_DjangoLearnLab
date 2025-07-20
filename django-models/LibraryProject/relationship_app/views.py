9b48c4d824eabd98808b405a15e33cfc792507ac
Elvis Ofori Nkrumah <elvisofori977@gmail.com>
2025-07-20 19:39:57 +0000

    Adding views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .models import Author, Book, Library, Librarian
 
-# Create your views here.
+
+def all_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/all_books.html', {'books': books})
+
+
+class LibraryDetailView:
+    def __init__(self, library_name):
+        self.library_name = library_name
+
+    def get_library(self):
+        return Library.objects.get(name=self.library_name)
+
+    def get_books(self):
+        return self.get_library().books.all()
+
+    def get_librarian(self):
+        return Librarian.objects.get(library=self.get_library())
+    
+    def render(self, request):
+        library = self.get_library()
+        books = self.get_books()
+        librarian = self.get_librarian()
+        return render(request, 'relationship_app/library_detail.html', {
+            'library': library,
+            'books': books,
+            'librarian': librarian
+        })
