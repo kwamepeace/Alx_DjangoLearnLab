967bc39786a038aa3c8eef264aef5cc864dad4ba
Elvis Ofori Nkrumah <elvisofori977@gmail.com>
2025-07-24 23:32:39 +0000

    Adding authentications

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('admin-dashboard/', views.admin_view, name='admin_dashboard'), 
     path('librarian-dashboard/', views.librarian_view, name='librarian_dashboard'), 
     path('member-dashboard/', views.member_view, name='member_dashboard'),
-       path('books/add/', views.add_book, name='add_book'),
-    path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
+    path('books/add_book/', views.add_book, name='add_book'),
+    path('books/edit_book/', views.edit_book, name='edit_book'),
     path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
 ]
