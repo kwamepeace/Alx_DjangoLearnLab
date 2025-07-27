3998bc45bed458038bc728ceb754ad3d098ff398
Elvis Ofori Nkrumah <elvisofori977@gmail.com>
2025-07-27 22:05:54 +0000

    Adding extra security to authenticate user

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager, User
 
 # Create your models here.
 
@@ -12,3 +12,28 @@ class CustomUser(AbstractUser):
     #A custom user model extending what is in AbsractUser without changing it
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        if not username:
+            raise ValueError('Users must have a username')
+
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        user = self.create_user(username, email, password)
+        user.is_admin = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
