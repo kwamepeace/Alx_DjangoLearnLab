6d4be4a887f1c212956395d5b6f9a847e0275cbd
Elvis Ofori Nkrumah <elvisofori977@gmail.com>
2025-07-30 01:46:46 +0000

    Updating files

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Book
+from django.contrib.auth.decorators import permission_required, login_required
 # Create your views here.
+
+
+@permission_required('bookshelf.can_view_book', raise_exception=True)
+@login_required
+def view_books(request):
+    # Logic to retrieve and display books
+    books = Book.objects.all()
+    return render(request, 'bookshelf/view_books.html', {'books': books})
+
+@permission_required('bookshelf.can_create_book', raise_exception=True)
+@login_required
+def create_book(request):
+    # Logic to create a new book
+    if request.method == 'POST':
+        # Handle form submission
+        pass
+    return render(request, 'bookshelf/create_book.html')
+
+@permission_required('bookshelf.can_edit_book', raise_exception=True)
+@login_required
+def edit_book(request, book_id):
+    # Logic to edit an existing book
+    book = Book.objects.get(id=book_id)
+    if request.method == 'POST':
+        # Handle form submission
+        pass
+    return render(request, 'bookshelf/edit_book.html', {'book': book})
+
+@permission_required('bookshelf.can_delete_book', raise_exception=True)
+@login_required
+def delete_book(request, book_id):
+    # Logic to delete a book
+    book = Book.objects.get(id=book_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('bookshelf:view_books')
+    return render(request, 'bookshelf/delete_book.html', {'book': book})
