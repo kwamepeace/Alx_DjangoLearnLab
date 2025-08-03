f84e36491beda38d46f325236a1f104c7271f921
Elvis Ofori Nkrumah <elvisofori977@gmail.com>
2025-08-03 22:09:20 +0000

    Setting up the serializer

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import BookSerializer
 
-# Create your views here.
+from .models import Book
+
+class BookList(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
