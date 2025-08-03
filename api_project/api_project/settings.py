018eff63b938b00354c1a9b1725d2be248356f54
Elvis Ofori Nkrumah <elvisofori977@gmail.com>
2025-08-03 19:32:42 +0000

    Adding django REST framework

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',  # Django REST Framework
+    'api.apps.ApiConfig',  
 ]
 
 MIDDLEWARE = [
