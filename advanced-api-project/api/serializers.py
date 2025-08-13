f23a9047a969adbe7c233e668670a4ff1d5405aa
Elvis Ofori Nkrumah <elvisofori977@gmail.com>
2025-08-13 01:21:01 +0000

    Adding a CRUD using DRF

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -8,12 +8,12 @@ class BookSerializer(serializers.ModelSerializer):
         model =  Book
         fields = ['title', 'publication_year', 'author']
 
-    def validate(self, data):
-        """
-        Custom validation to ensure publication year is not in the future.
-        """
-        if data['publication_year'] > 2025 and len(data['title']) < 5:
-            raise serializers.ValidationError("Publication year cannot be in the future.")
+    # def validate(self, data):
+    #     """
+    #     Custom validation to ensure publication year is not in the future.
+    #     """
+    #     if data['publication_year'] > 2025 and len(data['title']) < 5:
+    #         raise serializers.ValidationError("Publication year cannot be in the future.")
 
 # A nested serializer for the Author model to include related books
 class AuthorSerializer(serializers.ModelSerializer):
