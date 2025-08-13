4b7f8705c70a84d7dc96ef53a675c512e5c4ad80
Elvis Ofori Nkrumah <elvisofori977@gmail.com>
2025-08-13 00:48:08 +0000

    Adding a CRUD using DRF

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -6,7 +6,7 @@ class Author(models.Model):
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=250)
+    title = models.CharField(max_length=250, unique= True)
     publication_year = models.IntegerField()
     author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
 
---
+++ advanced-api-project/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+
+
+urlpatterns = [
+    path('list/', ListView.as_view(), name='book-list'),
+    path('detail/<int:pk>/', DetailView.as_view(), name='book-detail'),
+    path('create/', CreateView.as_view(), name='book-create'),
+    path('update/<str:title>/', UpdateView.as_view(), name='book-update'),
+    path('delete/<int:pk>/', DeleteView.as_view(), name='book-delete'),
+]
