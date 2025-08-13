cd0804ba45a03b7dbc2ed07d104b150f1ee4b5db
Elvis Ofori Nkrumah <elvisofori977@gmail.com>
2025-08-13 00:57:38 +0000

    Adding a CRUD using DRF

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,8 +5,8 @@ from .views import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 urlpatterns = [
     path('books/', ListView.as_view(), name='book-list'),
+    path('books/create/', CreateView.as_view(), name='book-create'),
+    path('books/update/<str:title>/', UpdateView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', DeleteView.as_view(), name='book-delete'),
     path('book/<int:pk>/', DetailView.as_view(), name='book-detail'),
-    path('create/', CreateView.as_view(), name='book-create'),
-    path('update/<str:title>/', UpdateView.as_view(), name='book-update'),
-    path('delete/<int:pk>/', DeleteView.as_view(), name='book-delete'),
 ]
