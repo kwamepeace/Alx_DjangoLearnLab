785a3076eb88ad07839581270af4d109faf6b41c
Elvis Ofori Nkrumah <elvisofori977@gmail.com>
2025-08-17 21:51:58 +0000

    Creating a complete blog API

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -5,6 +5,6 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    published_at = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now_add=True)
     
 
