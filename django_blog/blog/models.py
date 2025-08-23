028938dcd8dd450bf37e068bcc84d91f309489e5
Elvis Ofori Nkrumah <elvisofori977@gmail.com>
2025-08-23 15:37:17 +0000

    Making edits to meet checker requirements

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -21,7 +21,7 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField (auto_now_add= True)
 
     def __str__(self):
